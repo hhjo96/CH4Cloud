ffbd08d9934129dbd0cf7ebf6a699cbc3c0ba447
hhjo <43205471+hhjo96@users.noreply.github.com>
2026-02-02 12:19:47 +0900

    ING: LV2

--- src/main/java/com/example/ch4cloud/controller/MemberController.java
+++ src/main/java/com/example/ch4cloud/controller/MemberController.java
@@ -7,6 +7,7 @@ import com.example.ch4cloud.service.MemberService;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class MemberController {
 
     private final MemberService memberService;
@@ -21,12 +23,14 @@ public class MemberController {
 
     @PostMapping("/api/members")
     public ResponseEntity<MemberCreateDto.Response> createMember(@RequestBody MemberCreateDto.Request request) {
+        log.info("[API - LOG] {} ", "createMember");
         return ResponseEntity.status(HttpStatus.CREATED).body(memberService.create(request));
     }
 
 
     @GetMapping("/api/members/{id}")
     public ResponseEntity<MemberGetDto.Response> getOne(@PathVariable Long id) {
+        log.info("[API - LOG] {} ", "getOne");
         return ResponseEntity.ok(memberService.getMember(id));
     }
 
